e178b6d77647b141ae14c2646f781fd8ce357886
wintofon <107524618+wintofon@users.noreply.github.com>
2025-06-19 10:51:50 -0300

    Revert "Refactor CSS using ITCSS"

--- views/steps/auto/step3.php
+++ views/steps/auto/step3.php
@@ -160,7 +160,8 @@ try {
   <!-- Bootstrap 5 (CDN) -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet">
-  <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/main.css">
+
+  <link rel="stylesheet" href="/wizard-stepper_git/assets/css/steps/auto/step3.css">
 </head>
 <body>
   <main class="container py-4">
