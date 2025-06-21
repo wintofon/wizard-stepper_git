6d57d00d0ef4e906a938077d1e4553d28236f5bd
wintofon <107524618+wintofon@users.noreply.github.com>
2025-06-21 14:51:46 -0300

    Add PHPUnit setup and example test

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit2cb96070b56f4e03491cb5b0cfa9ebb7
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit2cb96070b56f4e03491cb5b0cfa9ebb7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit2cb96070b56f4e03491cb5b0cfa9ebb7', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit2cb96070b56f4e03491cb5b0cfa9ebb7
         call_user_func(\Composer\Autoload\ComposerStaticInit2cb96070b56f4e03491cb5b0cfa9ebb7::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit2cb96070b56f4e03491cb5b0cfa9ebb7::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
