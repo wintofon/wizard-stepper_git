7a24b89b68a3d4da2dc4d5a18df8201d3aebc1ce
wintofon <107524618+wintofon@users.noreply.github.com>
2025-06-23 09:24:53 -0300

    Update step6.php

--- views/steps/step6.php
+++ views/steps/step6.php
@@ -35,7 +35,7 @@ if (empty($_SESSION['csrf_token'])) {
 }
 $csrfToken = $_SESSION['csrf_token'];
 
-/* 4) Datos básicos */
+/* 4) Datos básicos 
 /* $tool   = ToolModel::getTool($pdo, $_SESSION['tool_table'] ?? '', $_SESSION['tool_id'] ?? 0) ?? [];
 $params = ExpertResultController::getResultData($pdo, $_SESSION) ?? [];
 
@@ -48,6 +48,7 @@ $vc   = number_format($params['vc0']   ?? 0, 1, '.', '');
 fz    = number_format($params['fz0']   ?? 0, 4, '.', '');
 */
 $embedded = defined('WIZARD_EMBEDDED') && WIZARD_EMBEDDED;
+  include __DIR__ . '/../partials/styles.php';
 ?>
 <!DOCTYPE html>
 <html lang="es"><head>
