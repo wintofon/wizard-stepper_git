18f2d95da216b37c7fab6dcd15c10d820bd78157
wintofon <107524618+wintofon@users.noreply.github.com>
2025-06-26 11:27:57 -0300

    Update step2.php

--- views/steps/manual/step2.php
+++ views/steps/manual/step2.php
@@ -130,7 +130,7 @@ if ($tool) {
   <?php
     $styles = [
       'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-      'assets/css/components/_step2.css',
+      'assets/css/components/strategy.css',
       'assets/css/objects/step-common.css',
     ];
     $embedded = defined('WIZARD_EMBEDDED') && WIZARD_EMBEDDED;
