b8bd2a5e55463c7824d9f70186bad46cc0721e09
wintofon <107524618+wintofon@users.noreply.github.com>
2025-07-01 00:16:20 -0300

    Update step6.js

--- assets/js/step6.js
+++ assets/js/step6.js
@@ -134,7 +134,7 @@
 
     // Curva agresiva para potencia
     const mapPower = r => {
-      const γ = 1;
+      const γ = 0.5;
       const rγ = Math.pow(r, γ);
       return Math.min(100, (100 * rγ) / (1 + rγ));
     };
